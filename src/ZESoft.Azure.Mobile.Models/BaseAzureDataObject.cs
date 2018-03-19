d9b8ccd4852b13745f3ea7991e6f6b03fc93fe57
Christopher Zumberge <chriszumberge@gmail.com>
2018-03-19 12:51:06 -0400

    finished project

---
+++ src/ZESoft.Azure.Mobile.Models/BaseAzureDataObject.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ZESoft.Azure.Mobile.Models
+{
+    /// <summary>
+    /// Base Data Object class with all the required properties for an Azure Data Object.
+    /// </summary>
+    /// <see cref="ZESoft.Azure.Mobile.Models.IAzureDataObject"/>
+    public abstract class BaseAzureDataObject : IAzureDataObject
+    {
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.Version]
+        public byte[] Version { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.CreatedAt]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.UpdatedAt]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.Deleted]
+        public bool Deleted { get; set; }
+    }
+}
---
+++ src/ZESoft.Azure.Mobile.Models/IAzureDataObject.cs
@@ -0,0 +1,15 @@
+﻿namespace ZESoft.Azure.Mobile.Models
+{
+    /// <summary>
+    /// Interface for implementing an Azure Data Object with the minimum number of required fields to work
+    /// with the data store classes.
+    /// </summary>
+    public interface IAzureDataObject
+    {
+        string Id { get; set; }
+
+        byte[] Version { get; set; }
+
+        bool Deleted { get; set; }
+    }
+}
